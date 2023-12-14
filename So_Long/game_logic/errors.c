ad8597e6d9142d62b3280c2dd0a51b9c0f08d838
Luca Venceslai <77976921+Fanzio@users.noreply.github.com>
2023-12-14 17:48:29 +0100

    Add files via upload

---
+++ So_Long/game_logic/errors.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prossi <prossi@student.42adel.org.au>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 17:33:01 by prossi            #+#    #+#             */
+/*   Updated: 2022/12/12 20:46:47 by prossi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static int	horizontalwall(t_complete *game)
+{
+	int	i;
+	int	j;
+
+	i = game->widthmap;
+	j = 0;
+	while (j < i)
+	{
+		if (game->map[0][i] == '1' && game->map[game->heightmap - 1][j] == '1')
+			return (0);
+		j++;
+	}
+	return (1);
+}
+
+static int	verticalwall(t_complete *game)
+{
+	int	height;
+	int	width;
+
+	height = 0;
+	width = game->widthmap;
+	while (height < game->heightmap)
+	{
+		if (!(game->map[height][0] == '1' && game->map[height][width - 1] == '1'))
+			return (0);
+		height++;
+	}
+	return (1);
+}
+
+static void	if_walls(t_complete *game)
+{
+	int	verticalwalls;
+	int	horizontalwalls;
+
+	verticalwalls = verticalwall(game);
+	horizontalwalls = horizontalwall(game);
+	if (!verticalwalls || !horizontalwalls)
+	{
+		printf("\nIn questa mappa mancano i muri\n");
+		exit_point(game);
+	}
+}
+
+static void	count_checker(t_complete *game, int height, int width)
+{
+	if (game->map[height][width] != '1' &&
+		game->map[height][width] != '0' &&
+		game->map[height][width] != 'P' &&
+		game->map[height][width] != 'E' &&
+		game->map[height][width] != 'C' &&
+		game->map[height][width] != '\n')
+	{
+		printf("\nErrore!%c\n", game->map[height][width]);
+		exit_point(game);
+	}
+	if (game->map[height][width] == 'C')
+			game->columncount++;
+	if (game->map[height][width] == 'P')
+			game->playercount++;
+	if (game->map[height][width] == 'E')
+			game->exitcount++;
+}
+
+void	character_valid(t_complete *game)
+{
+	int	height;
+	int	width;
+
+	height = 0;
+	while (height < game->heightmap - 1)
+	{
+		width = 0;
+		while (width <= game->widthmap)
+		{
+			count_checker(game, height, width);
+			width++;
+		}
+		height++;
+	}
+	if (!(game->playercount == 1 && game->columncount > 1
+			&& game->exitcount == 1))
+	{
+		printf("\nErrore\n");
+		printf("Problema con il giocatore, l'uscita o un oggetto collezionabile\n");
+		exit_point(game);
+	}
+}
+
+void	check_errors(t_complete *game)
+{
+	if_walls(game);
+	character_valid(game);
+}
